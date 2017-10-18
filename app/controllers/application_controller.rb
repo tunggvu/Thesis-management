4b0ef3670b5408285c0e282384787e4020b617f7
tunggvu <vutungtn95@gmail.com>
2017-10-18 14:25:23 +0700

    fix login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  before_action :logged_in_checkout
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "You must login first"
+      redirect_to login_path
+    end
+  end
+
+  def verify_user
+    load_user
+    redirect_to root_path unless @user == current_user
+  end
+
+  def verify_admin
+    redirect_to root_url unless current_user.admin?
+  end
 end
