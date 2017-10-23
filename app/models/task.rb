f6b310aa81513e80aa05fe3d585f90254df265dd
HoanKi <hoanki2212@gmail.com>
2017-10-24 00:07:38 +0700

    Reset theme

--- app/models/task.rb
+++
@@ -1,5 +0,0 @@
-class Task < ApplicationRecord
-  belongs_to :thesis
-
-  enum status: [:new, :progressing, :done ]
-end
--- app/models/thesis.rb
+++
@@ -1,5 +0,0 @@
-class Thesis < ApplicationRecord
-  belongs_to :user
-
-  enum status: [:pending, :approved, :rejected]
-end
